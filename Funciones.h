c5dc2ff821bb2857c51e92e0f0709e3dbcb0e270
vaanlato03 <vaanlato07@gmail.com>
2026-04-13 21:41:53 -0400

    Primer avance - no funcional aún

---
+++ Funciones.h
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void mostrarMenu();
+
+string pedirStrings();
+int pedirEnteros();
+
+void ingresarEjercicio();
+void actualizarEjercicio();
+void eliminarEjercicio();
+void consultarEjercicio();
+void buscarPorIntensidad();
+void generarRutina();
+
+void liberarMemoria();
+
+void GestorDeRutinas();
